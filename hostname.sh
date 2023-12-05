7405524c49ccc9d1ac60f4ae0b6875b0712be493
Aldo Tapia <105082687+AldoTapiaInnova@users.noreply.github.com>
2023-12-05 12:07:18 -0300

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Check if AWS CLI is installed
+if ! command -v aws &> /dev/null; then
+    echo "AWS CLI is not installed. Please install it and configure it before running this script."
+    exit 1
+fi
+
 # Fetching the token for AWS metadata API
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 
@@ -8,20 +16,49 @@ AVAILABILITY_ZONE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254
 REGION="${AVAILABILITY_ZONE%?}"
 
 # Fetching the AWS instance ID
-AWS_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+AWS_INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
 
-HOSTNAME=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | cut -f5)
-FORMATTED_HOSTNAME=$(echo $HOSTNAME | tr -d '()' | tr ' ' '-')
+# Attempt to fetch instance tags to check if metadata tags are enabled
+TEST_TAGS=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/tags/instance)
 
-read -p "Change hostname to $FORMATTED_HOSTNAME? [Y/n] " -n 1 -r
-echo
-
-if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]
-then
-    echo "Setting new hostname: $FORMATTED_HOSTNAME"
-    sudo hostnamectl set-hostname "$FORMATTED_HOSTNAME"
-
-    exec bash
+# Check if fetching tags was successful
+if [[ $TEST_TAGS == *"<Code>NotFound</Code>"* ]]; then
+    # Metadata tags are not enabled, prompt to enable
+    read -p "Instance metadata tags are not enabled. Enable them now? [Y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+        aws ec2 modify-instance-metadata-options --instance-id $AWS_INSTANCE_ID --instance-metadata-tags enabled
+        echo "Enabled access to instance metadata tags."
+        # Wait for changes to take effect
+        sleep 10
+    else
+        echo "Metadata tags not enabled. Script will exit."
+        exit 1
+    fi
 else
-    echo "Hostname change declined."
+    echo "Instance metadata tags are already enabled."
+fi
+
+# Fetching the hostname tag
+RAW_HOSTNAME_TAG=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_INSTANCE_ID" "Name=key,Values=Name" --output text | tr ' ' '-')
+
+# Extracting the hostname value from the tag output
+HOSTNAME=$(echo $RAW_HOSTNAME_TAG | awk '{print $5}')
+
+# Check if the hostname was retrieved and format it
+if [ -z "$HOSTNAME" ]; then
+    echo "No 'Name' tag found for this instance. Skipping hostname change."
+    exit 1
+else
+    FORMATTED_HOSTNAME=$(echo $HOSTNAME | tr -d '()' | tr ' ' '-')
+    # Prompt for hostname change
+    read -p "Change hostname to $FORMATTED_HOSTNAME? [Y/n] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+        echo "Setting new hostname: $FORMATTED_HOSTNAME"
+        sudo hostnamectl set-hostname "$FORMATTED_HOSTNAME"
+        exec bash
+    else
+        echo "Hostname change declined."
+    fi
 fi
